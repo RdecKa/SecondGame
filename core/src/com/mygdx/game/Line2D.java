060ce072a2841bb95ec072da35e5aa1ad7c9ab6b
karin <kf5121@student.uni-lj.si>
2017-09-12 22:31:32 +0000

    Add dots to end of lines.

--- core/src/com/mygdx/game/Line2D.java
+++ core/src/com/mygdx/game/Line2D.java
@@ -1,17 +1,31 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+
 public class Line2D extends Obstacle {
 	private Point2D A1, A2;
+	private float endPointRadius;
 
 	public Line2D(float red, float green, float blue, float alpha, Point2D A1, Point2D A2) {
 		super(red, green, blue, alpha);
 		this.A1 = A1;
 		this.A2 = A2;
+		this.endPointRadius = 3;
 	}
 
 	public void draw() {
+		// Draw line
 		GameEnvironment.clearModelMatrix();
 		LineGraphics.drawLine(A1, A2);
+
+		// Draw dots on the ends of line
+		Gdx.gl.glUniform4f(GameEnvironment.colorLoc, 0, 0, 0, 1);
+		GameEnvironment.setModelMatrixScale(2 * endPointRadius, 2 * endPointRadius);
+
+		GameEnvironment.setModelMatrixTranslation(A1.getPositionX(), A1.getPositionY());
+		CircleGraphics.drawSolidCircle();
+		GameEnvironment.setModelMatrixTranslation(A2.getPositionX(), A2.getPositionY());
+		CircleGraphics.drawSolidCircle();
 	}
 
 	public Point2D getA1() {
